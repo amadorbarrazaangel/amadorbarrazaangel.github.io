4b5ae6b64a13d1b03fbd4f124ef2b4faa0ccf395
amadorbarrazaangel <94088706+amadorbarrazaangel@users.noreply.github.com>
2022-03-20 18:46:37 -0700

    Update diseño_1°.php

--- diseño_1°.php
+++ diseño_1°.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Magical Navigation</title>
-	<link rel="stylesheet" type="text/css" href="Style.css">
+	<link rel="stylesheet" type="text/css" href="css/Style.css">
 </head>
 <body>
  <div class="navigation">
